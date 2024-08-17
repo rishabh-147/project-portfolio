ab65e7aad611189c2c3c9e693a336ae8c639bb91
rishabh-147 <onlinestuff.rishabh@gmail.com>
2024-08-17 22:44:05 +0530

    bug fixes

--- main/src/main/java/com/rishabh/main/repo/UserDetailsRepoInterface.java
+++ main/src/main/java/com/rishabh/main/repo/UserDetailsRepoInterface.java
@@ -1,7 +1,10 @@
 package com.rishabh.main.repo;
 
+import org.springframework.stereotype.Repository;
+
 import com.rishabh.main.domain.UserDetails;
 
+@Repository
 public interface UserDetailsRepoInterface {
 
 	public UserDetails getUserDetails(int userId);
